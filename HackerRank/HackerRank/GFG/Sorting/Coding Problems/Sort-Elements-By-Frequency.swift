66be4019178edcf0822f13bfb782f83a3d3eda82
Rahul Mahajan <rahul_mahajan@apple.com>
2018-03-24 11:21:43 +0530

    GFG: sort by frequency

--- HackerRank/HackerRank/GFG/Sorting/Coding Problems/Sort-Elements-By-Frequency.swift
+++ HackerRank/HackerRank/GFG/Sorting/Coding Problems/Sort-Elements-By-Frequency.swift
@@ -18,8 +18,31 @@ func sortByFrequency(arr : inout [Int]){
             dict[i] = 1
         }
     }
-    
-    print(dict)
+    var result = [Int]()
+    var count = dict.count
+    while count > 0 {
+        let key = findMaxKeyIn(dict: dict)
+        if let value = dict[key]{
+            for _ in 0..<value{
+                result.append(key)
+            }
+            dict.removeValue(forKey: key)
+            count = dict.count
+        }
+    }
+    print(result)
+}
+
+func findMaxKeyIn(dict : [Int:Int]) -> Int {
+    var maxValue = 0
+    var maxKey = 0
+    for (key,value) in dict{
+        if value >= maxValue {
+            maxValue = value
+            maxKey = key
+        }
+    }
+    return maxKey
 }
 
 
